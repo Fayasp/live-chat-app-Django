e420fb479b0124e6e63f00e4e3a0e7e3681a7cc2
Fayasp <fayasp007@gmail.com>
2025-03-25 22:25:19 +0530

    display numbers of online users

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -7,21 +7,30 @@ import json
 
 class ChatroomConsumer(WebsocketConsumer):
     def connect(self):
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>")
         self.user            = self.scope["user"]
         self.chatroom_name   = self.scope["url_route"]['kwargs']['chatroom_name']
-        print(":::::::::::::::::::::::::::::::::::", self.chatroom_name)
+
         self.chatroom        = get_object_or_404(ChatGroup, group_name = self.chatroom_name)
-        print(self.chatroom,">>>>>>>>>>>>>>>>>>>")
         async_to_sync(self.channel_layer.group_add)(
             self.chatroom_name,self.channel_name
         )
+
+        #add and update online user
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+
         self.accept()
 
     def disconnect(self, close_code):
          async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name,self.channel_name
         )
+        
+         # remove and update user
+         if self.user in self.chatroom.users_online.all():
+             self.chatroom.users_online.remove(self.user)
+             self.update_online_count()
         
 
     def receive(self, text_data):
@@ -51,4 +60,23 @@ class ChatroomConsumer(WebsocketConsumer):
         html   = render_to_string("partials/chat_message_p.html",context)
         self.send(text_data=html)
 
+    def update_online_count(self):
+        
+        online_count   = self.chatroom.users_online.count() - 1
+        event          = {
+            'type'         : 'online_count_handler',
+            'online_count' : online_count
+        }
+
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name,event)
+  
+    
+    def online_count_handler(self,event):
+
+        online_count  = event["online_count"]
+        context = {
+            "online_count" : online_count
+        }
+        html   = render_to_string("partials/online_count.html",context)
+        self.send(text_data=html)
 
